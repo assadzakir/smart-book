bf7eb185f1468040cd6af73244db44cd0605a353
Assad Zakir <assadzakirhussain@gmail.com>
2016-12-26 21:48:00 +0500

    Display Recipelist

--- dist/out-tsc/app/routing.js
+++ dist/out-tsc/app/routing.js
@@ -1,12 +1,14 @@
 import { RouterModule } from '@angular/router';
 import { ChefListComponent } from './chef-list/chef-list.component';
 import { ChefDetailsComponent } from './chef-details/chef-details.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 var indexRoute = { path: '', redirectTo: 'chef', pathMatch: 'full' };
 export var chefsRoutes = [
     { path: 'chef', component: ChefListComponent },
     { path: 'chef/:name', children: [
             { path: '', component: ChefDetailsComponent },
         ] },
+    { path: 'recipes', component: RecipeListComponent },
     indexRoute
 ];
 export var chefsRouting = RouterModule.forRoot(chefsRoutes);
--- src/app/routing.ts
+++ src/app/routing.ts
@@ -4,6 +4,7 @@
 import {Routes, RouterModule} from '@angular/router';
 import  {ChefListComponent} from './chef-list/chef-list.component'
 import  {ChefDetailsComponent} from './chef-details/chef-details.component'
+import  {RecipeListComponent} from './recipe-list/recipe-list.component'
 
 const indexRoute = {path: '', redirectTo: 'chef', pathMatch: 'full'};
 export const chefsRoutes: Routes = [
@@ -11,6 +12,7 @@ export const chefsRoutes: Routes = [
     {path: 'chef/:name', children: [
         {path: '', component: ChefDetailsComponent},
     ]},
+    {path: 'recipes', component: RecipeListComponent},
     indexRoute
 ];
 
